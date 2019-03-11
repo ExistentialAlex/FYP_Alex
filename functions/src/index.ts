145de862921c8ae230169ea688437e647ed1ceb4
Alex Hewitson <hewitsonalexander@gmail.com>
2019-03-11 16:21:19 +0000

    === #16 === Author: Alexander Hewitson Date: 11/03/2019
    Comments:
    - The function works!!!
    - Have updated the function to the latest version.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,111 +1,109 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as path from 'path';
+import * as WordExtractor from 'word-extractor';
+import * as textract from 'textract';
 import suffixArray from './suffixArray';
 
-interface Location {
-  lid: string;
-  location_name: string;
-  location_type: string;
-  sentimental_value: number;
-}
+// interface Location {
+//   lid: string;
+//   location_name: string;
+//   location_type: string;
+//   sentimental_value: number;
+// }
 
-interface Context {
-  lid: string;
-  context_string: string;
-  fid: string;
-}
+// interface Context {
+//   lid: string;
+//   context_string: string;
+//   fid: string;
+// }
 
 export const processFile = functions.storage.object().onFinalize(async file => {
-  const serviceAccount = require(__dirname + "/../config/serviceAccount.json");
+  const serviceAccount = require(__dirname + '/../config/serviceAccount.json');
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://fyp-alex.firebaseio.com"
+    databaseURL: 'https://fyp-alex.firebaseio.com',
   });
 
   const firestore = admin.firestore();
-  const settings = { timestampsInSnapshots: true };
-  firestore.settings(settings);
 
   const fileBucket: string = file.bucket;
   const filePath: string = file.name;
   const fileDet: string = path.basename(filePath);
-  const fileNameSplit: string[] = fileDet.split(".");
+  const fileNameSplit: string[] = fileDet.split('.');
   const fileExt: string = fileNameSplit.pop();
-  const fileName: string = fileNameSplit.join(".");
+  const fileName: string = fileNameSplit.join('.');
   const bucket = admin.storage().bucket(fileBucket);
   const fileRef = bucket.file(filePath);
   const _path: string = `/tmp/${fileName}.${fileExt}`;
-  console.log(`File path ${filePath}`);
-  console.log("Getting Download URL");
   try {
-    console.log(`Downloading to: ${_path}`);
     await fileRef.download({ destination: _path });
-    console.log("File Saved");
-    console.log(`Getting Details: ${_path}`);
+    console.log('File Saved');
     const text = await getText(_path, fileExt);
-    console.log(`Processing: ${fileName}`);
-    console.log("Creating Suffix Array");
+    console.log(`Text: ${text}`);
+    const searchTerms = await getSearchTerms();
+    console.log(`Search Terms: ${searchTerms}`);
+    console.log('Creating Suffix Array');
     const suffix_array = suffixArray(text);
-    console.log(`Suffix Array Created: ${suffix_array}`);
-    console.log("Getting Search Patterns");
-    const searchTerms: FirebaseFirestore.DocumentData[] = await getSearchTerms();
-    console.log('Search Patterns Received');
-    const promises = [];
-    const allContexts = [];
     for (const searchDoc of searchTerms) {
       const searchTerm = searchDoc.location_name.toLowerCase();
       console.log(searchTerm);
-      const matchedIndexes = search(text, searchTerm, suffix_array);
-      const contexts = createContexts(
-        matchedIndexes,
-        searchDoc,
-        text,
-        fileName
-      );
-      allContexts.concat(contexts);
+      const matchedIndexes = search(text.toLowerCase(), searchTerm, suffix_array);
+      console.log(matchedIndexes);
+      console.log(matchedIndexes.length);
+      if (matchedIndexes.length > 0) {
+        console.log('Creating Contexts');
+        const contexts = createContexts(matchedIndexes, searchDoc, text, fileName);
+        console.log(contexts);
+        const promises = [];
+        if (contexts.length > 0) {
+          console.log('Adding Contexts to DB');
+          for (const context of contexts) {
+            const p = admin
+              .firestore()
+              .collection('FYP_CONTEXTS')
+              .add(context);
+            promises.push(p);
+          }
+          await Promise.all(promises);
+          console.log('Contexts added to DB');
+        } else {
+          console.log('No contexts to add');
+        }
+      }
     }
-    for (const context of allContexts) {
-      const p = admin
-        .firestore()
-        .collection("FYP_CONTEXTS")
-        .add(context);
-      promises.push(p);
-    }
-    await Promise.all(promises);
     const data = {
-      processed: 1
+      processed: 1,
     };
     return firestore.doc(`FYP_FILES/${fileName}`).update(data);
   } catch (e) {
-    console.error(e);
-    const data = {
-      processed: 2
+    const failData = {
+      processed: 2,
     };
-    return firestore.doc(`FYP_FILES/${fileName}`).update(data);
+    return firestore
+      .doc(`FYP_FILES/${fileName}`)
+      .update(failData)
+      .catch(e => {
+        console.error(e);
+      });
   }
 });
 
 async function getText(_path: string, fileExt: string) {
-  let text: string = "";
+  let text: string = '';
 
   switch (fileExt) {
-    case "docx":
-    case "doc":
-      const WordExtractor = require("word-extractor");
+    case 'docx':
+    case 'doc':
       const extractor = new WordExtractor();
       const extracted = await extractor.extract(_path);
       text = extracted.getBody();
       break;
-    case "pdf":
+    case 'pdf':
       break;
-    case "txt":
-      const textract = require("textract");
-      textract.fromFileWithPath(_path, function(
-        extractedError,
-        string: string
-      ) {
+    case 'txt':
+      textract.fromFileWithPath(_path, function(extractedError: any, string: string) {
         if (extractedError) {
           console.error(extractedError);
         }
@@ -115,23 +113,43 @@ async function getText(_path: string, fileExt: string) {
       });
       break;
     default:
-      console.log("Unsupported File Type");
-      return null;
+      console.log('Unsupported File Type');
   }
   return text;
 }
 
 async function getSearchTerms(): Promise<FirebaseFirestore.DocumentData[]> {
-  const snapshot = await admin
-    .firestore()
-    .collection("FYP_LOCATIONS")
-    .get();
-  console.log('Get Returned')
-  return snapshot.docs.map(doc => doc.data());
+  try {
+    const snapshot = await admin
+      .firestore()
+      .collection('FYP_LOCATIONS')
+      .get();
+    return snapshot.docs.map(doc => doc.data());
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
-function createContexts(matchedIndexes, searchDoc, text, fileName) {
-  console.log("Creating Contexts");
+function search(text: string, search_term: string, suffix_array: number[]) {
+  const matches = [];
+  suffix_array.forEach(index => {
+    const substring = text.substring(index, index + search_term.length);
+    const match = search_term.localeCompare(substring);
+
+    if (match === 0) {
+      matches.push(index);
+    }
+  });
+  return matches;
+}
+
+function createContexts(
+  matchedIndexes: number[],
+  searchDoc: FirebaseFirestore.DocumentData,
+  text: string,
+  fileName: string
+) {
   const contexts = [];
   const searchTerm = searchDoc.location_name.toLowerCase();
   for (const index of matchedIndexes) {
@@ -147,41 +165,41 @@ function createContexts(matchedIndexes, searchDoc, text, fileName) {
     contexts.push({
       lid: searchDoc.lid,
       context_string: context,
-      fid: fileName
+      fid: fileName,
     });
   }
   return contexts;
 }
 
-function search(text: string, searchTerm: string, suffix_array: number[]) {
-  console.log(`Beginning search for: ${searchTerm}`);
-  let start = 0;
-  let end = suffix_array.length;
-  const matchedIndexes: Array<number> = [];
+// function search(text: string, searchTerm: string, suffix_array: number[]) {
+//   console.log(`Beginning search for: ${searchTerm}`);
+//   let start = 0;
+//   let end = suffix_array.length - 1;
+//   const matchedIndexes: number[] = [];
 
-  while (start < end) {
-    const mid: number = (end - 1) / 2;
-    const index: number = suffix_array[mid];
-    const finalIndex: number = index + searchTerm.length;
-    if (finalIndex <= text.length) {
-      const substring: string = text.substring(index, finalIndex);
-      const match: number = searchTerm.localeCompare(substring);
+//   while (start < end) {
+//     const mid: number = (end) / 2;
+//     const index: number = suffix_array[mid];
+//     const finalIndex: number = index + searchTerm.length;
+//     if (finalIndex <= text.length) {
+//       const substring: string = text.substring(index, finalIndex);
+//       const match: number = searchTerm.localeCompare(substring);
 
-      if (match === 0) {
-        console.log(`Match Found at Index: ${index}`);
-        matchedIndexes.push(index);
-      } else if (match < 0) {
-        end = mid;
-      } else if (match > 0) {
-        start = mid;
-      }
-      console.log(matchedIndexes);
-    }
-  }
+//       if (match === 0) {
+//         console.log(`Match Found at Index: ${index}`);
+//         matchedIndexes.push(index);
+//       } else if (match < 0) {
+//         end = mid;
+//       } else if (match > 0) {
+//         start = mid;
+//       }
+//       console.log(matchedIndexes);
+//     }
+//   }
 
-  if (matchedIndexes.length === 0) {
-    console.log(`No matches found for search term: ${searchTerm}`);
-  }
+//   if (matchedIndexes.length === 0) {
+//     console.log(`No matches found for search term: ${searchTerm}`);
+//   }
 
-  return matchedIndexes;
-}
+//   return matchedIndexes;
+// }
